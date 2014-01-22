75854f69032c819c36d65dcf54548344f7efee5b
Jim <Jim@Alpha.home>
2014-01-22 13:47:27 -0800

    Initial Commit

--- src/connect4/Connect4.java
+++ src/connect4/Connect4.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Java Project: Connect 4
+    Authors: MJ Lafond & Jim Cendejas
  */
 
 package connect4;
@@ -16,7 +15,7 @@ public class Connect4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Hello Partner!
     }
     
 }
