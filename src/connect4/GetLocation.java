9ec4c47915144ded98c955512ea7e4723331cebc
Kyle Eslick <mii756@gmail.com>
2014-02-13 21:37:25 -0700

    commented code in GitLocatoin

--- src/connect4/GetLocation.java
+++ src/connect4/GetLocation.java
@@ -14,34 +14,35 @@ import java.util.Scanner;
  */
 public class GetLocation {
     String userInput;
+    
     public String getLocation() {
         Scanner input = new Scanner(System.in);
-         this.userInput = input.next();
-         return userInput;
+        this.userInput = input.next();
+        return userInput;
     }
     
     public String askUser() {
         while(true) {
             System.out.print("Please enter a column to drop your piece: ");
             this.userInput = getLocation();
+            
             if (validateUserInput(this.userInput)) {
                return this.userInput;
             }
             System.out.println("Please enter a valid number(1-7)");
-        }
-        
+        }        
     }
     
     public Boolean validateUserInput(String userInput) {
        int test;
-        try {
-            test = Integer.parseInt(userInput);
-        }catch (NumberFormatException e) {
-            return false;
-        }
-        if (test < 1 || test > 7) {
-            return false;
-        }
-        return true;
+       try {
+           test = Integer.parseInt(userInput);//tries to make a number out of the users' input
+       }catch (NumberFormatException e) { // if it is not able to make a number it fails and returns false
+           return false;
+       }
+       if (test < 1 || test > 7) { // the number must be 1-7 if not it will be false
+           return false;
+       }
+       return true; 
     }
 }
